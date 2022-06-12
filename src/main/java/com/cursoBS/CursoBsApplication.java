cdf16798d5d043b868d568b9922dc23620890ae7
Hebert Brito <hbdev1211@gmail.com>
2022-06-12 20:33:00 -0300

    Project Created

---
+++ src/main/java/com/cursoBS/CursoBsApplication.java
@@ -0,0 +1,13 @@
+package com.cursoBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoBS/CursoBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
