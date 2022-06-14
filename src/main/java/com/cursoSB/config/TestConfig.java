e07958da7feb988f560c6197bf6581bfc20a1310
Hebert Brito <hbdev1211@gmail.com>
2022-06-14 20:44:10 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cursoSB/config/TestConfig.java
+++ src/main/java/com/cursoSB/config/TestConfig.java
@@ -32,7 +32,7 @@ public class TestConfig  implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	
 	@Autowired //ESSA ANOTAÇAO CRIA UMA DEPENDENCIA DA INTERFACE USERREPOSITORY;
-	private ProductRepository roductRepository;
+	private ProductRepository productRepository;
 	
 	
 
@@ -59,7 +59,23 @@ public class TestConfig  implements CommandLineRunner{
 		userRepositoty.saveAll(Arrays.asList(u1, u2));
 		orderRepositoty.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		roductRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategory().add(cat2);
+		p2.getCategory().add(cat1);
+		p2.getCategory().add(cat3);
+		p3.getCategory().add(cat3);
+		p4.getCategory().add(cat3);
+		p5.getCategory().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
