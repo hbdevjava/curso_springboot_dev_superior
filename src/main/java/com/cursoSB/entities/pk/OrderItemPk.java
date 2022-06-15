9923052ccce349208a990b2db914cbf759f08785
Hebert Brito <hbdev1211@gmail.com>
2022-06-15 09:08:49 -0300

    OrderItem, many-to-many association with extra attributes Part 1

---
+++ src/main/java/com/cursoSB/entities/pk/OrderItemPk.java
@@ -0,0 +1,64 @@
+package com.cursoSB.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+//SEMPRE QUE FOR PRECISO CRIAR UMA CLASSE AUX PRA SER UMA CHAVE PK COMPOSTA INSERE UM SUBPACOTE .pk;
+//ESSA CLASSE ESPECIAL NAO TEM CONSTRUTORES;
+import com.cursoSB.entities.Order;
+import com.cursoSB.entities.Product;
+
+
+@Embeddable
+public class OrderItemPk implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+// NO hashCode/equals SELECIONA OS DOIS ATRIBUTOS PARA SEREM COMPARADOS
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPk other = (OrderItemPk) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
+}
--- src/main/java/com/cursoSB/services/UserServices.java
+++ src/main/java/com/cursoSB/services/UserServices.java
@@ -1,4 +1,4 @@
-package com.cursoSB.services;
+ package com.cursoSB.services;
 
 import java.util.List;
 import java.util.Optional;
